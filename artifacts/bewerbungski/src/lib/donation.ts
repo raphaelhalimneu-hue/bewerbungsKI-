8181e8aa199a6061e5f7034711eff72591ddab26
Raphi <raphaelhalimneu@gmail.com>
2026-08-22 18:18:42 +0000

    Add voluntary PayPal donation link

--- artifacts/bewerbungski/src/lib/donation.ts
+++ artifacts/bewerbungski/src/lib/donation.ts
@@ -2,4 +2,5 @@
  * Replace this one value when the voluntary support page is ready.
  * Donations never control access to any BewerbungsKI feature.
  */
-export const DONATION_LINK = "DONATION_LINK";
+export const DONATION_LINK =
+  "https://www.paypal.com/donate/?business=Rosehalim%40aol.com&currency_code=EUR";
